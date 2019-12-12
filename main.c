069820e85f531b4808f44b2fce65a19eaaa488e8
Jason Wu <zwu00@stuy.edu>
2019-12-12 10:47:29 -0500

    mostly working but bugged redir system

--- main.c
+++ main.c
@@ -55,47 +55,70 @@ int main() {
     const char *more_p = &morethan;
     for(int q = 0; q < numCommands; q++) {//for every command entered
 
-	    char * pointer = commandArray[q];
+	  char * pointer = commandArray[q];
       char backup[100];
       strcpy(backup, commandArray);
       struct parse_output outstruct;
       outstruct = parse_args(input);
       outstruct.lastToken--;
-      //int has_greater = strchr(outstruct.output[outstruct.lastToken-1], ">");//has a ">"
-      //int has_lessthan = strchr(outstruct.output[outstruct.lastToken-1], "<");//has a "<"
+
+      //get pointers to where < and > are in the output char**
+      int less_num = -1;
+      int more_num = -1;
+      for (int counter = 0; counter < outstruct.lastToken; counter++) {
+        if (strcmp(outstruct.output[counter], "<") == 0) {
+          printf("Found a <\n");
+          less_num = counter;
+        }
+        if (strcmp(outstruct.output[counter], ">") == 0) {
+          printf("Found a >\n");
+          less_num = counter;
+        }
+      }
 
       printf("DEBUG: your cmd: %s, last_token: %i\n", commandArray[q], outstruct.lastToken);
       //printf("Command has > sign: %i, 0 if DNE\n", strchr(outstruct.output, morethan) != NULL);
-      //printf("DEBUG: %s\n", backup[q]);
       //commented out printf that caused segfault
       if (strstr(commandArray[q], cd_p) != NULL) {//input command has a "cd" in it
         chdir(outstruct.output[1]);
         printf("\nWISH > ");
       }
+
       else if (strstr(commandArray[q], leave_p) != NULL) {//input command is "exit"
         printf("\nWISH > Exiting shell\nThank you for visiting! Come again soon!\n\n");
         exit(0);
       }
-      else if (strchr(commandArray[q], morethan) != NULL) {//has a ">" or a "<"
-        printf("DEBUG: value of last token: %s\n", outstruct.output[outstruct.lastToken]);
-        int fd_redirectedFile = open(outstruct.output[outstruct.lastToken], O_RDWR);//to see if file exists
-        int new_fd_stdout = dup(1);
-        int new_fd_stding = dup(0);
 
-        if (fd_redirectedFile == -1) {//if no such file
-          printf("DEBUG: file DNE\n");
-          printf("DEBUG: fd_redir has this value: %i\n", fd_redirectedFile);
-          //create a file
-          fd_redirectedFile = open(outstruct.output[outstruct.lastToken], O_RDWR | O_CREAT);
-          //redirection below
-          //dup2()
-          exit(0);
+      else if (less_num != -1 || more_num != -1) {//has a ">" or a "<"
+        printf("DEBUG: value of last token: %s\n", outstruct.output[outstruct.lastToken]);
+        int fd_new_input = open(outstruct.output[less_num--], O_RDWR);//to see if file exists
+        int fd_new_output = open(outstruct.output[more_num++], O_RDWR);//to see if file exists
+        int new_fd_stdout = dup(1);
+        int new_fd_stdin = dup(0);
+
+        if (fd_new_input == -1) {//if no input file exists
+          printf("DEBUG: input file DNE\n");
+          printf("DEBUG: fd input has this value: %i\n", fd_new_input);
+          int fd_new_input = open(outstruct.output[less_num--], O_RDWR | O_CREAT);
         }
-        else {//file exists, proceed with redirection
-          printf("DEBUG: file exists\n");
-          printf("DEBUG: fd_redir has this value: %i\n", fd_redirectedFile);
-          exit(0);
+        if (fd_new_output == -1) {//no output file exists
+          printf("DEBUG: output file DNE\n");
+          printf("DEBUG: fd output has this value: %i\n", fd_new_output);
+          int fd_new_output = open(outstruct.output[more_num++], O_RDWR | O_CREAT);
         }
+        printf("DEBUG: less_num: %i, more_num: %i\n", less_num, more_num);
+        if (less_num != -1) {
+          dup2(fd_new_input, 0);//replaces stdin with new input file
+          printf("stdin replaced\n");
+        }
+        if (more_num != -1) {
+          dup2(fd_new_output, 1);//now with stdout
+          printf("stdout replaced\n");
+        }
+        run_cmds(outstruct, parentPID);
+        dup2(new_fd_stdin, 0);
+        dup2(new_fd_stdout, 1);//undoes file table manipulation
+        printf("DEBUG: Redirection done\n");
       }
       else {
         run_cmds(outstruct, parentPID);
